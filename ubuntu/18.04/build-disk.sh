62d456d4d77ca4fea9a058529a0d2248d931b6af
Mauro Delazeri <maurodelazeri@gmail.com>
2019-01-04 10:54:52 -0500

    new stuff

--- ubuntu/18.04/build-disk.sh
+++ ubuntu/18.04/build-disk.sh
@@ -9,13 +9,12 @@ set -e
 RAM_SIZE=${1:-"2048"}
 DISK_SIZE=${2:-"10G"}
 DISK_FORMAT=${3:-"qcow2"}
-SSH_PUBLIC_KEY_FILE=${4:-"$HOME/.ssh/id_rsa.pub"}
 DISK_FILE=${5:-"`pwd`/ubuntu-18.04-amd64-$RAM_SIZE-$DISK_SIZE.$DISK_FORMAT"}
 
 # create iso
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TMP_ISO_DIR="`mktemp -d`"
-eval "$SCRIPT_DIR/build-iso.sh" "$SSH_PUBLIC_KEY_FILE" "$TMP_ISO_DIR/ubuntu-18.04-netboot-amd64-unattended.iso"
+eval "$SCRIPT_DIR/build-iso.sh" "$TMP_ISO_DIR/ubuntu-18.04-netboot-amd64-unattended.iso"
 
 # create image and run installer
 "$BIN_QEMU_IMG" create "$DISK_FILE" -f "$DISK_FORMAT" "$DISK_SIZE"
--- ubuntu/18.04/build-iso.sh
+++ ubuntu/18.04/build-iso.sh
@@ -7,15 +7,7 @@ set -e
 : "${BIN_CPIO:=$(type -P gnucpio || type -P cpio)}"
 
 # get parameters
-SSH_PUBLIC_KEY_FILE=${1:-"$HOME/.ssh/id_rsa.pub"}
 TARGET_ISO=${2:-"`pwd`/ubuntu-18.04-netboot-amd64-unattended.iso"}
-
-# check if ssh key exists
-if [ ! -f "$SSH_PUBLIC_KEY_FILE" ];
-then
-    echo "Error: public SSH key $SSH_PUBLIC_KEY_FILE not found!"
-    exit 1
-fi
 
 # get directories
 CURRENT_DIR="`pwd`"
@@ -39,8 +31,6 @@ patch -p1 -i "$SCRIPT_DIR/custom/boot-menu.patch"
 cd "$TMP_INITRD_DIR"
 mkdir "./custom"
 cp "$SCRIPT_DIR/custom/preseed.cfg" "./preseed.cfg"
-cp "$SSH_PUBLIC_KEY_FILE" "./custom/userkey.pub"
-cp "$SCRIPT_DIR/custom/ssh-host-keygen.service" "./custom/ssh-host-keygen.service"
 
 # append assets to initrd image
 cd "$TMP_INITRD_DIR"
