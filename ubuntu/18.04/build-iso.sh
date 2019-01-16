9b28449009bf45c981d6538a2921f58c783d4c74
Mauro Delazeri <maurodelazeri@gmail.com>
2019-01-16 13:14:49 -0500

    new stuff

--- ubuntu/18.04/build-iso.sh
+++ ubuntu/18.04/build-iso.sh
@@ -22,19 +22,14 @@ wget -4 "$SOURCE_ISO_URL" -O "./netboot.iso"
 "$BIN_7Z" x "./netboot.iso" "-o$TMP_DISC_DIR"
 
 # patch boot menu
-cd "$TMP_DISC_DIR"
-dos2unix "./isolinux.cfg"
-patch -p1 -i "$SCRIPT_DIR/custom/boot-menu.patch"
-
-cd "$TMP_DISC_DIR"
-dos2unix "./isolinux.cfg"
-patch -p1 -i "$SCRIPT_DIR/custom/boot-menu.patch"
-
+# cd "$TMP_DISC_DIR"
+# dos2unix "./isolinux.cfg"
+# patch -p1 -i "$SCRIPT_DIR/custom/boot-menu.patch"
 
 # prepare assets
-# cd "$TMP_INITRD_DIR"
-# mkdir "./custom"
-# cp "$SCRIPT_DIR/custom/preseed.cfg" "./preseed.cfg"
+cd "$TMP_INITRD_DIR"
+mkdir "./custom"
+cp "$SCRIPT_DIR/custom/preseed.cfg" "./preseed.cfg"
 
 cp "$SCRIPT_DIR/custom/splash.png" "$TMP_DISC_DIR/splash.png"
 cp "$SCRIPT_DIR/custom/menu.cfg" "$TMP_DISC_DIR/menu.cfg"
